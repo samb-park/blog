3300fcf904a21c311e8dec3e7f31f8aa2b6fbc49
Sangbong Park <sangbong.park@outlook.com>
2024-05-07 18:04:37 -0400

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,19 @@
 import '../public/bootstrap-4.4-2.1-dist/css/bootstrap.css';
 
-import {
-  Route,
-  createBrowserRouter,
-  createRoutesFromElements,
-  RouterProvider,
-} from 'react-router-dom';
+import { HashRouter as Router, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Resume from './pages/Resume';
 import MainLayout from './layouts/MainLayout';
 
 function App() {
-  const router = createBrowserRouter(
-    createRoutesFromElements(
-      <Route path='/' element={<MainLayout />}>
-        <Route index element={<Home />} />
-        {/* <Route path='project' element={<Project />} />
-        <Route path='experience' element={<Experience />} />
-        */
-        <Route path='resume' element={<Resume />} /> }
-      </Route>
-    )
-  );
-
   return (
-      <RouterProvider router={router} />
+    <Router>
+      <MainLayout>
+        <Route path="/" exact component={Home} />
+        <Route path="/resume" component={Resume} />
+        {/* Other routes */}
+      </MainLayout>
+    </Router>
   );
 }
 
